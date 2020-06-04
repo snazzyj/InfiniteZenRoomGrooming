b5e7635121fe894331e878160ce523f958953c19
AJ Olson <silentx.alex@gmail.com>
2020-06-04 14:47:48 -0400

    added about content, added services content

--- src/services/services.js
+++ src/services/services.js
@@ -1,9 +1,71 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Services extends Component {
     render() {
         return (
-            <h1>Hello from Services</h1>
+            <section>
+                <h1>Infinite Zen Room Grooming LLC + Pet Care A La Carte Packaging</h1>
+                <h3>Providing The Best Care For Your Best Friends</h3>
+
+                <h3>Grooming:</h3>
+                <ul>
+                    <li>Nail Trim $12</li>
+                    <li>A La Carte Anal Glands $18</li>
+                    <li>Bath and Blow Dry(short hair and smaller breeds) $20-$90	</li>
+                    <li>Blowout (double coated and large breeds) $50-$95</li>
+                    <li>Brush $10-$25</li>
+                    <p>note:grooming costs will vary depending on your pet’s breed and coat condition</p>
+                </ul>
+
+                <h3>Overnight Stays:</h3>
+                <ul>
+                    <li>$55(up to 2 dogs) per night $12 per additional dog</li>
+                    <li>4th dog is free</li>
+                    <li>Overnight stay includes:
+        <ul>
+                            <li>2 potties per day</li>
+                            <li>Unlimited let outs when I am there</li>
+                            <li>AM and PM feedings</li>
+                            <li>$5 per additional feeding</li>
+                            <li>Fresh water change at anytime it is needed</li>
+                        </ul>
+                    </li>
+                    <li>$5 for medicating per day</li>
+                </ul>
+
+
+                <h3>Additional Pet Care:</h3>
+
+                <ul>
+                    <li>$8 per Day For The Following Pets:
+            <ul>
+                            <li>Lizards</li>
+                            <li>Snakes</li>
+                            <li>Guinea Pigs</li>
+                            <li>Hamsters</li>
+                            <li>Gerbils</li>
+                            <li>Birds</li>
+                            <li>Fish</li>
+                            <li>Chickens</li>
+                        </ul>
+
+                    </li>
+
+                    <li>Includes:
+        <ul>
+                            <li>Feeding</li>
+                            <li>Watering</li>
+                            <li>Bedding Change(if necessary)</li>
+                            <li>Water Change(does NOT include salt water; fresh water only)</li>
+                            <li>Handling(if possible or requested)</li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <h3>Mileage:</h3>
+                <p>$0.55/mile outside the greater Madison area.</p>
+
+            </section>
         )
     }
 }
