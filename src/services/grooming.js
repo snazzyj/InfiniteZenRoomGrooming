ed3d68c7bbc26f61c549f5c42d7114cc7ed2acb0
AJ Olson <silentx.alex@gmail.com>
2020-06-15 15:27:27 -0400

    seperated services into 2 seperate files and tabs

---
+++ src/services/grooming.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import './services.css';
+
+class Grooming extends Component {
+    render() {
+        return (
+           <section className="services">
+                <h3>Providing The Best Care For Your Best Friends</h3>
+
+                <h3>Grooming</h3>
+                <ul>
+                    <li>Nail Trim $12</li>
+                    <li>A La Carte Anal Glands $18</li>
+                    <li>Bath and Blow Dry(short hair and smaller breeds) $20-$90	</li>
+                    <li>Blowout (double coated and large breeds) $50-$95</li>
+                    <li>Brush $10-$25</li>
+                    <p>note:grooming costs will vary depending on your pet’s breed and coat condition</p>
+                </ul>
+           </section> 
+        )
+    }
+}
+
+export default Grooming
--- src/services/petsitting.js
+++ src/services/petsitting.js
@@ -1,23 +1,11 @@
-import React, { Component } from 'react';
-import './services.css'
+import React, {Component} from 'react';
+import './services.css';
 
-class Services extends Component {
+class AdditionalServices extends Component {
     render() {
         return (
             <section className="services">
-                <h1>Infinite Zen Room Grooming LLC + Pet Care A La Carte Packaging</h1>
-                <h3>Providing The Best Care For Your Best Friends</h3>
-
-                <h3>Grooming</h3>
-                <ul>
-                    <li>Nail Trim $12</li>
-                    <li>A La Carte Anal Glands $18</li>
-                    <li>Bath and Blow Dry(short hair and smaller breeds) $20-$90	</li>
-                    <li>Blowout (double coated and large breeds) $50-$95</li>
-                    <li>Brush $10-$25</li>
-                    <p>note:grooming costs will vary depending on your pet’s breed and coat condition</p>
-                </ul>
-
+                <h1>Pet Care A La Carte Packaging</h1>
                 <h3>Overnight Stays</h3>
                 <ul>
                     <li>$55(up to 2 dogs) per night $12 per additional dog</li>
@@ -65,10 +53,9 @@ class Services extends Component {
 
                 <h3>Mileage</h3>
                 <p>$0.55/mile outside the greater Madison area.</p>
-
             </section>
         )
     }
 }
 
-export default Services
+export default AdditionalServices;
