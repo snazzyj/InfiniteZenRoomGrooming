df471705c86ff6525096a70ac5c7f9bb92c087fb
AJ Olson <silentx.alex@gmail.com>
2020-10-26 11:38:32 -0400

    updated about me. added secondary photo

--- src/about/about.js
+++ src/about/about.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import ProfilePic from '../assests/images/profile.png'
+import ProfilePic from '../assests/images/profile.png';
+import Selfie from '../assests/images/selfie.jpg';
 
 class About extends Component {
   constructor(props) {
@@ -28,8 +29,8 @@ class About extends Component {
           )}
         Jenn Joyce!
         </h1>
-        <img src={ProfilePic} alt="Jennifer Joyce" />
         <div className="aboutContent">
+        <img src={ProfilePic} alt="Jennifer Joyce" className="profilePic"/>
 
           <p>
             As an animal lover of all shapes and sizes, Jenn has years of experience and has developed a passion working with dogs  since she was very young.
@@ -41,7 +42,7 @@ class About extends Component {
             Although she loved to be surrounded by over 100 dogs in a play yard all day, she knew it wasn’t enough.
             She wanted the one on one time with dogs to give them the love and TLC to make them happy and their owners happier!
           </p>
-
+          <img src={Selfie} alt="Selfie with Murphy" className="selfiePic"/>
           <p>
             Jenn discovered her grooming career in 2014, but it didn’t take off until 2015 when Jenn was gifted with the opportunity to run a grooming business with her grooming partner and friend.
             Later, she went to work at a small shop in Oregon to work with a groomer who she shadowed in her high school years.
@@ -49,7 +50,6 @@ class About extends Component {
             While working at one of the top grooming salons in Madison, she decided that her dream of owning a grooming salon was about to kick off.
             Having more than 6 years of grooming under her belt, Jenn’s knowledge is strong and her confidence is growing.
           </p>
-
           <p>
             Making dogs and cats look their best is what she lives for! Giving them the best experience is what is most important to her.
             Ensuring that both the owner and the pet are calm, happy and zen  is priority to Jenn.
