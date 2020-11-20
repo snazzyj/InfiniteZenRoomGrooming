e3849a98b32cfb085bd034502e31688a689594f1
AJ Olson <silentx.alex@gmail.com>
2020-11-20 10:07:22 -0500

    added 2 new dog photos

--- src/gallery/images.js
+++ src/gallery/images.js
@@ -3,9 +3,12 @@ import Dog2 from '../assests/dogs/dog2.jpg';
 import Dog3 from '../assests/dogs/dog3.jpg';
 import Dog4 from '../assests/dogs/dog4.jpg';
 import Dog5 from '../assests/dogs/dog5.jpg';
+import Dog6 from '../assests/dogs/dog6.jpg';
+import Dog7 from '../assests/dogs/dog7.jpg';
 import Dog8 from '../assests/dogs/dog8.jpg';
 import Dog9 from '../assests/dogs/dog9.jpg';
 import Dog10 from '../assests/dogs/dog10.jpg';
+import Dog11 from '../assests/dogs/dog11.jpg';
 import Dog13 from '../assests/dogs/dog13.jpg';
 import Dog15 from '../assests/dogs/dog15.jpg';
 import Dog17 from '../assests/dogs/dog17.jpg';
@@ -17,7 +20,7 @@ import Dog24 from '../assests/dogs/dog24.jpg';
 
 const ImageData = [
     {
-        text: 'Default Alt Text',
+        text: 'Murphy Side View',
         image: Dog1
     },
     {
@@ -38,6 +41,14 @@ const ImageData = [
     },
     {
         text: 'Default Alt Text',
+        image: Dog6
+    },
+    {
+        text: 'Default Alt Text',
+        image: Dog7
+    },
+    {
+        text: 'Default Alt Text',
         image: Dog8
     },
     {
@@ -47,6 +58,10 @@ const ImageData = [
     {
         text: 'Default Alt Text',
         image: Dog10
+    },
+    {
+        text: 'Default Alt Text',
+        image: Dog11
     },
     {
         text: 'Default Alt Text',
